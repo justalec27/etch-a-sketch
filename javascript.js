e866df6c551ac592e3e68bcc77ab11bf60d7792f
Alec <215294797+justalec27@users.noreply.github.com>
2025-07-18 19:18:45 +0200

    change background color when hover over element
    This adds a playful element to the game. Background color will change to black when hover over it

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container")
-
-
+// container.addEventListener("mouseover", (event) => {
+//     container.style.backgroundColor = "red"
+// })
 
 for ( let i = 0 ; i < 256 ; i++){
     let newBox = document.createElement("div")
@@ -9,8 +10,17 @@ for ( let i = 0 ; i < 256 ; i++){
     }
 
 
-    
-    
+
+let colorBoxes = document.querySelectorAll(".square")
+
+colorBoxes.forEach((box) => {
+    box.addEventListener("mouseover", (event) => {
+        box.style.backgroundColor = "black"
+    box.addEventListener("mouseleave", (event) => {
+        box.style.backgroundColor = "white"
+         })
+    })
+})   
 
 
 
